1990bab67eed97ad2f8794b7faee58cd05aff0cd
”““wjzzzzzzzzzz” <450850664@qq.com>
2019-02-22 10:08:54 +0800

    modified:   leet/isPalindrome.go 	modified:   leet/reverse.go 	leet/addTwoNumbers.go 	leet/longestCommonPrefix.go 	leet/main.go 	leet/mergeTwoLists.go 	leet/twoSum.go

--- leet/isPalindrome.go
+++ leet/isPalindrome.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 )
 
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
 func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
@@ -19,5 +20,5 @@ func isPalindrome(x int) bool {
 	}
 }
 func main() {
-
+	isPalindrome(123)
 }
--- leet/reverse.go
+++ leet/reverse.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"log"
+	"math"
 	"strconv"
 )
 
 //给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转
 func reverse(x int) int {
-	if x > 0 {
-		s := []byte(strconv.Itoa(x))
-		log.Println(s)
+	var i int
+	s := []byte(strconv.Itoa(x))
+	if x >= 0 {
+		l := len(s) - 1
+		r := make([]byte, len(s))
 		for i, v := range s {
-			log.Println(i, v)
-			s[len(s)-i-1] = v
-
+			r[l-i] = v
 		}
-		i, _ := strconv.Atoi(string(s))
+		i, _ = strconv.Atoi(string(r))
+	} else {
+		s = s[1:]
+		l := len(s) - 1
+		r := make([]byte, len(s))
+		for i, v := range s {
+			r[l-i] = v
+		}
+		i, _ = strconv.Atoi("-" + string(r))
 
+	}
+	pow := int(math.Pow(float64(2), float64(31)))
+	if i < pow && (i+pow) >= 0 {
 		return i
 	}
 	return 0
-
 }
 func main() {
-	i := reverse(123)
+	i := reverse(1534236469)
 	log.Println(i)
 }
